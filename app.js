8d237248f5207894b543f0adc4cc4a9cb3480fab
Pranjal Sharma <psharma165@my.bcit.ca>
2025-11-27 23:30:05 -0800

    Small fix for mobile view

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const stickersData = [
     { file: "5.png", x: 350, y: 200, size: 200 },
     { file: "6.png", x: 170, y: 130, size: 110 },
     { file: "7.png", x: 20, y: 485, size: 130 },
-    { file: "8.png", x: 290, y: 380, size: 70 },
+    { file: "8.png", x: 310, y: 380, size: 70 },
     { file: "9.png", x: 210, y: 540, size: 160 },
     { file: "10.png", x: 295, y: 450, size: 155 },
     { file: "11.png", x: 10, y: 598, size: 85 },
-    { file: "12.png", x: 150, y:585, size: 80 }
+    { file: "12.png", x: 150, y:405, size: 80 }
 ];
 
 const wrapper = document.querySelector('.stickers-wrapper');
