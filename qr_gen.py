71dc9caf53761cf075e0f0f765f3358bdb94d2d2
Pranjal Sharma <psharma165@my.bcit.ca>
2026-01-08 14:09:22 -0800

    add QR code generator

---
+++ qr_gen.py
@@ -0,0 +1,18 @@
+import qrcode
+
+# The URL of your hosted frontend
+url = "https://mood-o-meter.onrender.com/"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("app_qr_code.png")
